0845203598ccf046adf6b84a75a0ed855074bdca
Soyeong Kim <soyeong.kim.95@gmail.com>
2021-07-09 22:13:30 +0900

    [feat] add cards to index page
    Signed-off-by: Soyeong Kim <soyeong.kim.95@gmail.com>

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import requests
+import xmltodict, json
 
 app = Flask(__name__)
 API_KEY = "4c63018be1fc4d10a6118f339347226f"
@@ -7,7 +8,11 @@ API_KEY = "4c63018be1fc4d10a6118f339347226f"
 # HTML 화면 보여주기
 @app.route("/")
 def main():
-    return render_template('index.html')
+    r = requests.get('http://www.kopis.or.kr/openApi/restful/pblprfr?service=4c63018be1fc4d10a6118f339347226f&stdate=20210601&eddate=20210731&cpage=1&rows=10&prfstate=02&signgucode=11&signgucodesub=1111')
+    obj = xmltodict.parse(r.text)
+    data = json.dumps(obj['dbs']['db'], ensure_ascii=False)
+    # print(obj_json)
+    return render_template('index.html', data=data)
 
 @app.route("/performance")
 def performance():

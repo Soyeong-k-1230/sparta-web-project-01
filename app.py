df47ecf141ff6eefa585a7ee5e33f59bdcbe767d
Soyeong Kim <soyeong.kim.95@gmail.com>
2021-07-16 11:21:40 +0900

    [feat] add guest book page
    Signed-off-by: Soyeong Kim <soyeong.kim.95@gmail.com>

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import os
 import requests
 import xmltodict, json
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from dotenv import load_dotenv
 from datetime import date
+from pymongo import MongoClient
 
 load_dotenv(verbose=True)
 app = Flask(__name__)
 API_KEY = os.getenv('API_KEY')
 BASE_URL = 'http://www.kopis.or.kr/openApi/restful/pblprfr?service=' + API_KEY
+
+client = MongoClient('localhost', 27017)
+db = client.dbarts
 
 # HTML 화면 보여주기
 @app.route("/")
@@ -34,9 +38,31 @@ def performance():
 def recommend():
     return render_template('recommend.html')
 
+@app.route("/board")
+def board():
+    return render_template('board.html')
+
 # API 역할을 하는 부분
-def get_performance_list():
-    return ''
+@app.route('/comment', methods=['POST'])
+def write_comment():
+    name = request.form['name']
+    content = request.form['content']
+    today = date.today()
+
+    obj = {
+        'name': name,
+        'content': content,
+        'date': today.strftime("%Y/%m/%d")
+    }
+    db.comment.insert_one(obj)
+    return jsonify({'msg': '저장이 완료되었습니다!'})
+
+@app.route('/comment', methods=['GET'])
+def read_comments():
+    comments = list(db.comment.find({}, {'_id': False}))
+    return jsonify({'all_comments': comments})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
